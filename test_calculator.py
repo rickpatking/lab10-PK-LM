e479beaded6b62090a1e21932d1d60eac64f5156
Logan Mears <logan.mears@ufl.edu>
2025-04-09 10:53:38 -0400

    committing test changes

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,26 @@ from calculator import *
 class TestCalculator(unittest.TestCase):
     # Partner 2
     def test_add(self): # 3 assertions
-        pass
+        expected = 7
+        actual = add(3, 4)
+        self.assertEqual(expected, actual)
+        expected = -3
+        actual = add(-3, 0)
+        self.assertEqual(expected, actual)
+        expected = 17
+        actual = add(9, 8)
+        self.assertEqual(expected, actual)
 
     def test_subtract(self): # 3 assertions
-        pass
+        expected = -1
+        actual = subtract(3, 4)
+        self.assertEqual(expected, actual)
+        expected = 4
+        actual = subtract(8, 4)
+        self.assertEqual(expected, actual)
+        expected = 0
+        actual = subtract(4, 4)
+        self.assertEqual(expected, actual)
     ##########################
 
     # Partner 1
@@ -21,16 +37,24 @@ class TestCalculator(unittest.TestCase):
     # Partner 2
     def test_divide_by_zero(self): # 1 assertion
         # call division function inside, example:
-        # with self.assertRaises(<INSERT_ERROR_TYPE>):
-        #     div(0, 5)
-        pass
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        pass
+        expected = 2
+        actual = logarithm(10, 100)
+        self.assertEqual(expected, actual)
+        expected = 2
+        actual = logarithm(2, 4)
+        self.assertEqual(expected, actual)
+        expected = 2
+        actual = logarithm(3, 9)
+        self.assertEqual(expected, actual)
 
     def test_log_invalid_base(self): # 1 assertion
         # use same technique from test_divide_by_zero
-        pass
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
     ##########################
     
     # Partner 1
