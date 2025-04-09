45597a2e306b045152384e234bf1f6183d4330e6
rickpatking <patrickking@ufl.edu>
2025-04-09 10:21:51 -0400

    changed calculator.py

--- calculator.py
+++ calculator.py
@@ -4,3 +4,28 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    if a < 0 or b < 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exponent(a, b):
+    if a < 0:
+        raise ValueError
+    return math.pow(a, b)
